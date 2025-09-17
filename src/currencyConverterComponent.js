8ea3ffd2ef72e86caf4070cc1345212a20db76ee
Satish Kuppuswamy <115114532+satish1373@users.noreply.github.com>
2025-09-17 16:26:40 -0500

    TES-146: Update currencyConverterComponent.js to include JPY support

---
+++ src/currencyConverterComponent.js
@@ -0,0 +1,8 @@
+// Updated currency converter component to display JPY
+import React from 'react';
+
+const CurrencyConverterComponent = () => {
+  return <div>Select currency: USD, EUR, JPY</div>;
+};
+
+export default CurrencyConverterComponent;
